afd34b47fe22fcfae618c65edf5b07d0c08a1339
ujangaris <ujangaja@gmail.com>
2021-02-10 18:08:43 +0700

    Make Users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = App\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.test',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
